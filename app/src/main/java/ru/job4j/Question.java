8857175f06e55431c05bfffbb629ccb26e04d4ff
Ihor Sapronov <islimoff@gmail.com>
2019-10-27 14:26:03 +0200

    Fix the buttons

---
+++ app/src/main/java/ru/job4j/Question.java
@@ -0,0 +1,43 @@
+package ru.job4j;
+
+import java.util.List;
+
+public class Question {
+
+    private int id;
+    private String text;
+    private List<Option> options;
+    private int answer;
+    private  int currentAnswer;
+
+    public Question(int id, String text, List<Option> options, int answer) {
+        this.id = id;
+        this.text = text;
+        this.options = options;
+        this.answer = answer;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public List<Option> getOptions() {
+        return options;
+    }
+
+    public int getAnswer() {
+        return answer;
+    }
+
+    public void setCurrentAnswer(int currentAnswer) {
+        this.currentAnswer = currentAnswer;
+    }
+
+    public int getCurrentAnswer() {
+        return currentAnswer;
+    }
+}
