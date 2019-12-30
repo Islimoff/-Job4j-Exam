4c3e5634f382bc7b256724589326924cb9e15a5c
Ihor Sapronov <islimoff@gmail.com>
2019-12-30 20:28:47 +0200

    add menu items

--- app/src/main/java/ru/job4j/ExamsActivity.java
+++ app/src/main/java/ru/job4j/ExamsActivity.java
@@ -1,7 +1,12 @@
 package ru.job4j;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -21,6 +26,26 @@ public class ExamsActivity extends AppCompatActivity {
         recycler = findViewById(R.id.exams);
         recycler.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
         updateUI();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.exams,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.add_item:
+                Toast.makeText(ExamsActivity.this,"ADD",Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.delete_item:
+                Toast.makeText(ExamsActivity.this,"DELETE",Toast.LENGTH_SHORT).show();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private void updateUI() {
